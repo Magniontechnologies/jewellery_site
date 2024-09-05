7b59d6a6054f87dccc86557377fc5f26c276c7b3
ZX 404 <149653325+kishor404@users.noreply.github.com>
2024-09-05 11:24:25 +0530

    Navigation

--- jewellerysite/src/Components/Navbar.js
+++ jewellerysite/src/Components/Navbar.js
@@ -1,8 +1,31 @@
+// import { Link } from "react-router-dom";
+
 const Navbar=()=>{
     return(
         <>
         <section>
-            <p>Navbar</p>
+            <div>
+                {/* LOGO HERE */}
+                <img/>
+
+            </div>
+            <div>
+                {/* --- FOR OVER PAGE NAVIGATION --- */}
+
+                {/* <Link to='/'>Home</Link>
+                <Link to="/About">About</Link>
+                <Link to='/Services'>Services</Link>
+                <Link to='/Gallery'>Gallery</Link>
+                <Link to'/Contact'>Contact</Link> */}
+
+                {/* --- FOR SINGLE PAGE NAVIGATION --- */}
+
+                <a href="#Home">Home</a>
+                <a href="#About">About</a>
+                <a href="#Services">Services</a>
+                <a href="#Gallery">Gallery</a>
+                <a href="#Contact">Contact</a>
+            </div>
         </section>
         </>
     )
--- jewellerysite/src/Pages/Home.js
+++ jewellerysite/src/Pages/Home.js
@@ -1,8 +1,20 @@
 const Home=()=>{
     return(
         <>
-        <section>
-            <p>Body</p>
+        <section id="Home" style={{height:'100vh'}}>
+            <p>Home</p>
+        </section>
+        <section id="About" style={{height:'100vh'}}>
+            <p>About</p>
+        </section>
+        <section id="Services" style={{height:'100vh'}}>
+            <p>Services</p>
+        </section>
+        <section id="Gallery" style={{height:'100vh'}}>
+            <p>Gallery</p>
+        </section>
+        <section id="Contact" style={{height:'100vh'}}>
+            <p>Contact</p>
         </section>
         </>
     )
