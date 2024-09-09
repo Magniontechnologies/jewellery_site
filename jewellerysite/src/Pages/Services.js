7dd9e5de95b72cd2184ae665d3d4b663a8b3fabe
Gopikarn071 <gopikaranr@outlook.com>
2024-09-09 10:08:41 +0530

    service page updated

--- jewellerysite/src/Pages/Services.js
+++ jewellerysite/src/Pages/Services.js
@@ -1,8 +1,21 @@
+import './Styles/Services.css'
+
 const Services=()=>{
     return(<>
-        <section id="Services" style={{height:'90vh'}}>
-            <p>Services</p>
-        </section>
+        <div className="services-container">
+      <div className="service-box">
+        <h2>Custom Jewelry Creation</h2>
+        <p>Let us bring your dream designs to life with our expert jewelry-making services. Whether it's an engagement ring or a special piece for a loved one, we craft unique, personalized jewelry tailored to your style and preferences.</p>
+      </div>
+      <div className="service-box">
+        <h2>Gemstone Procurement</h2>
+        <p>At Amirthaasri, we source only the highest quality gemstones from trusted suppliers. Our gemologists carefully select each stone for its brilliance, clarity, and uniqueness, ensuring every piece we offer is truly remarkable.</p>
+      </div>
+      <div className="service-box">
+        <h2>Diamond Grading</h2>
+        <p>We offer professional diamond grading and certification, ensuring the authenticity and quality of every diamond we sell. Our expertise in diamond evaluation guarantees that you receive a gem of impeccable quality.</p>
+      </div>
+    </div>
     </>)
 }
 export default Services;
