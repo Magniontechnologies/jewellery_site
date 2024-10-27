a7b30e6fa48738b9fb9034704175719022cc1d1e
Kishor404 <149653325+Kishor404@users.noreply.github.com>
2024-10-27 12:31:19 +0530

    bug fix

--- jewellerysite/src/Pages/About.js
+++ jewellerysite/src/Pages/About.js
@@ -13,7 +13,7 @@ const About=()=>{
                 <div className='About-R'>
                     <p className='About-Title'>| About Us</p>
                     <div className='About-p'>
-                        <p>At Amirthaasri Gems & Jewellers, we are dedicated to crafting timeless jewelry that celebrates beauty and sophistication. With decades of expertise in custom jewelry making, gemstone sourcing, and diamond grading, we pride ourselves on delivering pieces that are both elegant and unique. Our commitment to quality and customer satisfaction has earned us a reputation as one of the leading jewelers in the region.</p>
+                        <p>At Amirthaasri Gems & Jewellers, We are dedicated to crafting timeless jewelry that celebrates beauty and sophistication. With decades of expertise in custom jewelry making, gemstone sourcing, and diamond grading, we pride ourselves on delivering pieces that are both elegant and unique. Our commitment to quality and customer satisfaction has earned us a reputation as one of the leading jewelers in the region.</p>
                         <p>From personalized jewelry creations to expert gem selection, each piece is carefully designed and crafted to perfection. Whether you're seeking a one-of-a-kind engagement ring or professional diamond certification, Amirthaasri offers unparalleled service and craftsmanship.</p>
                     </div>
                     <a href="#Gallery">Show Collections</a>
